99a4c9c444d39ca616c09a0a16f82363c6468711
JPLRepo <jamieleighton69@gmail.com>
2020-03-04 14:13:49 +1000

    couple of files I missed

---
+++ JSITransparentPods/GameEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace JSIAdvTransparentPods
+{
+    class JSIATPGameEvents
+    {
+        /// <summary>
+        /// Fires when JSI ATP Resets the Internal Model on a part.
+        /// </summary>
+        public static EventData<Part> onATPResetIVA;
+        /// <summary>
+        /// Fires when the player changes a JSI ATP pod setting ON/OFF/AUTO
+        /// </summary>
+        public static EventData<Part, string> onATPPodSettingChanged;
+    }
+}
